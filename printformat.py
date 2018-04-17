7fe88c3690f2c5bc6f67644248c2390b23501f64
sghosh246 <sghosh246@gmail.com>
2018-04-17 14:23:56 +0530

    Add files via upload

---
+++ printformat.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 16 14:08:03 2018
+@author: Sourav Ghosh
+"""
+string = "WE, THE PEOPLE OF INDIA,{}having solemnly resolved to constitute India into a SOVEREIGN,{}SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC{}and to secure to all its citizens{}"
+print(string.format('\n\t','!\n\t\t','\n\t\t',':'))
